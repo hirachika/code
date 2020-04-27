4b1a5076c33355fcce0273715c64b166e6abfcca
chikakohiraoka <zhijiazip@gmail.com>
2020-04-27 15:56:49 +0900

    JavaScript総合課題2：formで入力された誕生日から現在の年齢を計算して表示する

--- javascriprt_test/js/general_1.js
+++ javascriprt_test/js/general_1.js
@@ -1,64 +1,64 @@
 'use strict';
 
 //要素の準備
-  const weekly = ["日", "月", "火", "水", "木", "金", "土"];
-  const value  = document.querySelectorAll('.value');
+  const WEEKLY = ["日", "月", "火", "水", "木", "金", "土"];
   const button = document.querySelector('.button');
+  const value  = document.querySelectorAll('.value');
   const result = document.querySelector('.result');
   const error  = document.querySelector('.error');
 
-  // ボタン初期状態無効
+  // ボタン初期状態（無効）
   button.disabled = 'disabled';
 
-  // エラーメッセージ
-  let validation = (errorMessage) => {
-    error.innerHTML = errorMessage;
+  // エラーメッセージの表示と無効化
+  const showMessage = (message) => {
+    error.innerHTML = message;
+    button.disabled = 'disabled';
   }
 
   // エラーメッセージの解除とボタンの有効化
-  let clearAction = () => {
+  const hideMessage = () => {
     error.innerHTML = '';
-    button.disabled  = '';
+    button.disabled = '';
   }
 
   // 入力チェック
-  let check = (year,month,date) => {
-    // 共通エラーチェック
+  let validateInputs = (year,month,date) => {
     if(year < 1000 || year > 9999 || date < 1 || date > 31 || month < 1 || month > 12){
-      validation('空白または入力に誤りがあります。');
+      showMessage('空白または入力に誤りがあります。');
     }
     // 30日の月チェック
     else if(month === 4 && date > 30 || month === 6 && date > 30 || month === 9 && date > 30 || month === 11 && date > 30){
-      validation('空白または入力に誤りがあります。');
+      showMessage('1～30の半角数字で入力してください。');
     }
     // 2月チェック
     else if(year % 4 !== 0 && month === 2 && date > 28){
-      validation('1～28の半角数字で入力してください。');
+      showMessage('1～28の半角数字で入力してください。');
     }
     // 閏年チェック
-    else if(year % 4 === 0 && month === 2 && date > 29){
-      validation('閏年です。1～29の半角数字で入力してください。');
+    else if((year % 4 === 0 && year % 100 !== 0 && month === 2 && date > 29) || (year % 400 === 0 && month === 2 && date > 29 )){
+      showMessage('閏年です。1～29の半角数字で入力してください。');
     }
     else{
-      clearAction();
+      hideMessage();
     }
   }
 
-  value.forEach(target => {    
+  value.forEach(target => {
     target.onblur = () => {
-      let thisValue  = target.value;
-      let yearValue  = Number(value[0].value);
-      let monthValue = Number(value[1].value);
-      let dateValue  = Number(value[2].value);
+      const thisValue  = target.value;
+      const yearValue  = Number(value[0].value);
+      const monthValue = Number(value[1].value);
+      const dateValue  = Number(value[2].value);
 
       if(thisValue.match(/[^0-9]+/)){
-        validation('半角数字で入力してください。');
+        showMessage('半角数字で入力してください。');
       }
       else if(yearValue !== '' && monthValue !== '' && dateValue !== ''){
-        check(yearValue,monthValue,dateValue);
+        validateInputs(yearValue,monthValue,dateValue);
       }
       else{
-        clearAction();
+        hideMessage();
       }
     }
   });
@@ -66,6 +66,6 @@
   //クリックしたら曜日が出力
   button.addEventListener('click',(e) => {
     e.preventDefault();
-    let inputDay      = new Date(year.value + "/" + month.value + "/" + date.value);
-    result.innerHTML  = '＼その日は' + weekly[inputDay.getDay()] + '曜日です／';
+    const inputDay    = new Date(year.value + "/" + month.value + "/" + date.value);
+    result.innerHTML  = 'その日は' + WEEKLY[inputDay.getDay()] + '曜日です！';
   });
