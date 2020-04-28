2c9cee99af94bb22f60b18ca7584f230065dd3a0
chikakohiraoka <zhijiazip@gmail.com>
2020-04-28 14:25:49 +0900

    JavaScript総合課題1の修正：テンプレートリテラル、閏年判定の修正

--- javascriprt_test/js/general_1.js
+++ javascriprt_test/js/general_1.js
@@ -22,37 +22,6 @@
     BUTTON.disabled = '';
   }
 
-  // 入力チェック
-  const validateInputs = (year,month,date) => {
-    if(year < 1000 || year > 9999 || date < 1 || date > 31 || month < 1 || month > 12){
-      showMessage('空白または入力に誤りがあります。');
-    }
-    // 30日の月判定
-    else if(month.match(/(4|6|9|11)/) && date > 30){
-      showMessage('1～30の半角数字で入力してください');
-    }
-    else if(month === 2){
-      閏年();
-    }
-    else{
-      console.log('eureka');
-      hideMessage();
-    }
-  }
-
-  const validateLeapYear = (y,d) => {
-    // console.log(`閏年判定へよこうそ`);
-    if((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0) && d > 29){
-      showMessage('閏年です。1～29の半角数字で入力してください');
-    }
-    else{
-      // hideMessage();
-      console.log(`閏年ではありません`);
-      // break;
-    }
-  }
-
-
   VALUE.forEach(input => {
     input.onblur = () => {
       // 文字列で判定するもの
@@ -61,17 +30,26 @@
       const day   = VALUE[2].value;
       const date  = `${year}/${month}/${day}`;
 
-      if(input.value.match(/[^0-9]+/) || !date.match(/^\d{4}\/\d{1,2}\/\d{1,2}$/)){
-        showMessage('空白または入力に誤りがあります。');
-      }      
       // else if(year < 1000 || year > 9999 || day < 1 || day > 31 || month < 1 || month > 12){
-      // 2月 && 29日以上であれば閏年判定へ
-      else if(month == 2 && day > 28){
-        validateLeapYear(year,day);
+      if(input.value.match(/[^0-9]+/) || !date.match(/^\d{4}\/\d{1,2}\/\d{1,2}$/)){
+        showMessage('空白または入力に誤りがあります');
       }
-      // else if(month.match(/(4|6|9|11)/)){
-      //   showMessage('1～30の半角数字で入力してください');
-      // }
+      // 30日の月判定
+      else if(month.match(/(4|6|9|11)/) && date > 30){
+        showMessage('1～30の半角数字で入力してください');
+      }
+      // 2月判定
+      else if(month == 2 && day > 28) {
+        // 29日以上は閏年判定
+        if(day > 29) {
+          if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+            showMessage('閏年です。1～29の半角数字で入力してください');
+          }
+          else {
+            showMessage('1～28の半角数字で入力してください');
+          }
+        }
+      }
       else{
         hideMessage();
       }
@@ -81,6 +59,6 @@
   //クリックしたら曜日が出力
   BUTTON.addEventListener('click',(e) => {
     e.preventDefault();
-    const inputDay    = new Date(year.value + "/" + month.value + "/" + date.value);
-    RESULT.innerHTML  = 'その日は' + WEEKLY[inputDay.getDay()] + '曜日です！';
+    const inputDate  = new Date(`${year.value}/${month.value}/${date.value}`);
+    RESULT.innerHTML = `その日は${WEEKLY[inputDate.getDay()]}曜日です！`;
   });
