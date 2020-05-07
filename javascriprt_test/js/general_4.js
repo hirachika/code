cc79d55844bd2033709a0340657988ee85aed79d
chikakohiraoka <zhijiazip@gmail.com>
2020-05-08 00:54:44 +0900

    JS総合課題：正規表現の修正

--- javascriprt_test/js/general_4.js
+++ javascriprt_test/js/general_4.js
@@ -26,34 +26,34 @@
       const inputName  = input.name;
       e.preventDefault();
       if (inputName === 'user' && inputValue.match(/[^a-zA-Z0-9]/)) {
-        showMessage('');
+        showMessage();
       }
       else if (inputName === 'user' && 6 > inputValue.length || inputValue.length > 32) {
         showMessage('6〜32文字以内で入力してください');
       }
-      else if (inputName === 'password' && 8 > inputValue.length) {
+      else if (inputName === 'password' && inputValue.match(/[^a-zA-Z0-9]/)) {
         showMessage();
       }
-      else if (inputName === 'password' && inputValue.match(/[^a-zA-Z0-9]/)) {
+      else if (inputName === 'password' && 8 > inputValue.length) {
         showMessage('8文字以上で入力してください');
       }
       else if (inputName === 'passwordConfirm' && INPUT_VALUE[1].value !== INPUT_VALUE[2].value) {
         showMessage('入力されたパスワードが一致しません');
       }
-      else if (inputName === 'email-first' && !inputValue.match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*/)) {
-        showMessage('メールアドレスの前半に誤りがあります');
+      else if (inputName === 'email-first' && !inputValue.match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])/)) {
+        showMessage('メールアドレスに誤りがあります');
       }
       else if (inputName === 'email-last' && !inputValue.match(/^([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/)) {
-        showMessage('メールアドレスの後半に誤りがあります');
+        showMessage('メールアドレスに誤りがあります');
       }
-      else if (inputName === 'tel-first' && !inputValue.match(/^0\d{1,4}/)) {
-        showMessage('電話番号の入力に誤りがあります1');
+      else if (inputName === 'tel-first' && !inputValue.match(/^0\d{1,4}$/)) {
+        showMessage('電話番号の入力に誤りがあります');
       }
-      else if (inputName === 'tel-middle' && !inputValue.match(/\d{1,4}/)) {
-        showMessage('電話番号の入力に誤りがあります2');
+      else if (inputName === 'tel-middle' && !inputValue.match(/^\d{1,4}$/)) {
+        showMessage('電話番号の入力に誤りがあります');
       }
-      else if (inputName === 'tel-last' && !inputValue.match(/\d{3,4}$/)) {
-        showMessage('電話番号の入力に誤りがあります3');
+      else if (inputName === 'tel-last' && !inputValue.match(/\d{4}$/)) {
+        showMessage('電話番号の入力に誤りがあります');
       }
       else{
         hideMessage();
