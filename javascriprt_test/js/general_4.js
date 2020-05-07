081f694d0c4ebf1839e361b725ac7dda015f29bb
chikakohiraoka <zhijiazip@gmail.com>
2020-05-07 18:42:13 +0900

    JS総合課題：CSS調整、if文修正など

--- javascriprt_test/js/general_4.js
+++ javascriprt_test/js/general_4.js
@@ -1,86 +1,62 @@
 'use strict';
 
   // 初期設定
-  const user     = document.getElementById('user');
-  const tel      = document.querySelectorAll('.tel');
-  const mail     = document.querySelectorAll('.mail');
-  const password = document.querySelectorAll('.password');
-  const button   = document.querySelector('button');
+  const ERROR  = document.getElementsByClassName('error');
+  const BUTTON = document.getElementsByClassName('button');
+  BUTTON.disabled = 'disabled';
 
-  // エラーメッセージの作成
-  let p = document.createElement('p');
-      p.className = 'error';
-
-  // エラー時の処理
-  function errorAction(target){
-    p.innerHTML = '入力内容を確認してください';
-    target.closest('.form__list').appendChild(p);
-    button.disabled   = "disabled";
+  const VALUE  = document.getElementsByClassName('value');
+  const INPUT_VALUE = Array.from(VALUE);
+  
+  // エラーメッセージの表示と無効化
+  const showMessage = (message = '半角英数で入力してください') => {  
+    ERROR[0].innerHTML  = message;
+    BUTTON[0].disabled  = 'disabled';
   }
 
-  function clearAction(){
-    p.innerHTML     = '';
-    button.disabled = '';
+  // エラーメッセージの解除とボタンの有効化
+  const hideMessage = () => {
+    ERROR[0].innerHTML = '';
+    BUTTON[0].disabled = '';
   }
 
-  // ユーザー名
-  user.addEventListener('blur',function(){
-    let value = this.value;
-    if(6 > value.length || value.length > 32 || value.match(/[^a-zA-Z0-9]/)){
-      errorAction(this);
-    }
-    else{
-      clearAction();
-    }
-  },false);
-
-  // パスワード
-  for (const i of password.keys()) {
-    password[i].addEventListener('blur',function(){
-      let value = this.value;
-      if(8 > value.length || value.match(/[^a-zA-Z0-9]/)){
-        errorAction(this);
+  INPUT_VALUE.forEach(input => {
+    input.onblur = (e) => {
+      const inputValue = input.value;
+      const inputName  = input.name;
+      e.preventDefault();
+      if (inputName === 'user' && inputValue.match(/[^a-zA-Z0-9]/)) {
+        showMessage('');
+      }
+      else if (inputName === 'user' && 6 > inputValue.length || inputValue.length > 32) {
+        showMessage('6〜32文字以内で入力してください');
+      }
+      else if (inputName === 'password' && 8 > inputValue.length) {
+        showMessage();
+      }
+      else if (inputName === 'password' && inputValue.match(/[^a-zA-Z0-9]/)) {
+        showMessage('8文字以上で入力してください');
+      }
+      else if (inputName === 'passwordConfirm' && INPUT_VALUE[1].value !== INPUT_VALUE[2].value) {
+        showMessage('入力されたパスワードが一致しません');
+      }
+      else if (inputName === 'email-first' && !inputValue.match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*/)) {
+        showMessage('メールアドレスの前半に誤りがあります');
+      }
+      else if (inputName === 'email-last' && !inputValue.match(/^([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/)) {
+        showMessage('メールアドレスの後半に誤りがあります');
+      }
+      else if (inputName === 'tel-first' && !inputValue.match(/^0\d{1,4}/)) {
+        showMessage('電話番号の入力に誤りがあります1');
+      }
+      else if (inputName === 'tel-middle' && !inputValue.match(/\d{1,4}/)) {
+        showMessage('電話番号の入力に誤りがあります2');
+      }
+      else if (inputName === 'tel-last' && !inputValue.match(/\d{3,4}$/)) {
+        showMessage('電話番号の入力に誤りがあります3');
       }
       else{
-        clearAction();
-        password[1].addEventListener('blur',function(){
-          if(password[0].value !== password[1].value){
-            p.innerHTML = 'パスワードが一致していません';
-          }
-          else{
-            clearAction();
-          }
-        });
+        hideMessage();
       }
-    },false);
-  };
-
-  // メールアドレス
-  mail.forEach(i => {
-    i.addEventListener('blur',function(){
-      let value = this.value;
-      if(value === '' || value.match(/[^a-zA-Z0-9_.-]/)){
-        errorAction(this);
-      }
-      else{       
-        clearAction();
-      }
-    },false);
+    }
   });
-
-  // 電話番号
-  for (const i of tel.keys()) {
-    tel[i].addEventListener('blur',function(){
-      let value = this.value;
-      let valueTotal = tel[0].value+tel[1].value+tel[2].value;
-      if(value === '' || value.match(/[^0-9]/)){
-        errorAction(this);
-      }
-      else if(valueTotal.match(/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/)){
-        errorAction(this);
-      }
-      else{
-        clearAction();
-      }
-    },false);
-  };
