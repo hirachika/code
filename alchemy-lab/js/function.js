c6c469c732f0d808a9f7a317187d87d657ee2975
chikakohiraoka <zhijiazip@gmail.com>
2020-05-08 18:47:42 +0900

    グローバルメニューの改修

--- alchemy-lab/js/function.js
+++ alchemy-lab/js/function.js
@@ -1,20 +1,22 @@
 //navigation
-$(function() {
-    var $header = $('#header');
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > 350) {
-            $header.addClass('fixed');
-        } else {
-            $header.removeClass('fixed');
-        }
-    });
-    $('.headerButton').click(function(){
-        $header.toggleClass('open');
-    });
-    $('.globalNavList li a').click(function(){
-        $header.removeClass('open');
-    });
-});
+(function($) {
+  var $body   = $('body');
+  var $btn   = $('.toggle_btn');
+  var $mask  = $('#mask');
+  var open   = 'open'; // class
+  // menu open close
+  $btn.on( 'click', function() {
+    if ( ! $body.hasClass( open ) ) {
+      $body.addClass( open );
+    } else {
+      $body.removeClass( open );
+    }
+  });
+  // mask close
+  $mask.on('click', function() {
+    $body.removeClass( open );
+  });
+} )(jQuery);
 
 //scroll
 $(function(){
